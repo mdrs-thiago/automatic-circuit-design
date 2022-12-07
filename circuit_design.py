345984e90c800cacc85e317123e817c54558fd60
Thiago <thiago.medeiros.carvalho@gmail.com>
2022-12-07 17:32:26 -0300

    new function to transient analysis

--- circuit_design.py
+++ circuit_design.py
@@ -1,7 +1,8 @@
 from optimizer import GeneticAlgorithmPyGAD as GA
 from ltspice_circuits import Circuit
+import numpy as np 
 
-def fitness_function_parametrizable(components):
+def fitness_function_parametrizable(components:list, operation:str = '.op V(2)', node:str='V(2)', signal_ref = 2.5):
     def fitness_function(solution, solution_idx):
         common_components = ('R','L','C')
         list_components = ['Optimized circuit \n']
@@ -17,23 +18,32 @@ def fitness_function_parametrizable(components):
 
             list_components.append(__component)
 
-        list_components.append('.op V(2)\n')
+        list_components.append(f'{operation}\n')
         list_components.append('.end')
         netcir = Circuit('get_data') 
         netcir.generate_cir(list_components) 
         netcir.run_cir()
 
         #Transformar essa parte em uma nova função para ajustar aos mais 'comuns'.
-        res = netcir.read_output('V(2)')
-        return op_diff(res[0], 2.5)
+        
+        res = netcir.read_output(node)
+        type_op = operation.split()[0]
+        if type_op.contains('op'):
+            return op_diff(res[0], signal_ref)
+        elif type_op.contais('tran'):
+            return vector_diff(res, signal_ref)
+        
     return fitness_function
+
+def vector_diff(res, ans):
+    return -np.mean(abs(res - ans))
 
 
 def op_diff(res: float, ans: float):
     '''
     Diferença absoluta em um ponto de operação 
     '''
-    return abs(res - ans)
+    return -abs(res - ans)
 
 
 def write_component(component, sol):
@@ -53,6 +63,8 @@ def write_component(component, sol):
 if __name__ == '__main__':
     components = [('V1','1','0','5'),('R1','1','2'),('R2','2','0')]
 
-    ga = GA(ngenes = 2, fitness_function = fitness_function_parametrizable(components), 
+    fitness_function = fitness_function_parametrizable(components, operation='.op V(2)', node='V(2)')
+
+    ga = GA(ngenes = 2, fitness_function = fitness_function, 
             gene_space=[{'low':100,'high':1000},{'low':1000,'high':10000}])
     ga.run()
