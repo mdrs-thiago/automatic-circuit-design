1c0fc03b5041feb205fca5c809df492690ccebaa
Thiago <thiago.medeiros.carvalho@gmail.com>
2022-12-09 17:47:33 -0300

    add short circuit evaluation

--- revalidate_netlist.py
+++ revalidate_netlist.py
@@ -10,23 +10,47 @@ class Revalidator:
             if v == 1:
                 self.list_components.append(f'R99{k} {k} 0 10')
 
+    def check_short_circuit_transistor(self):
+        for v in self.list_components:
+            if v.startswith('Q'):
+                __v = np.array(v.split())
+                terminals, counts = np.unique(__v[1:4], return_counts=True)
+                sct = {t:c for t,c in zip(terminals, counts) if c > 1}
+
+                if len(sct) > 0:
+                    new_list = copy(self.list_components)   
+                    for t,c in sct.items():
+                        
+                        while c > 1:
+                            new_terminal = f'N{t}{str(c)}'
+                            __v[np.argwhere(__v==t)[0]] = new_terminal
+                            print(new_terminal)
+                            
+                            print(__v)
+                            new_list = [' '.join(__v) if k == v else k for k in new_list]
+                            new_list.append(f'RQ{c} {new_terminal} {t} 1000')
+                            c -= 1
+
+                    self.list_components = new_list
+
     def check_parallel_sources(self):
         v_sources = np.array([c for c in self.list_components if c.startswith('V')])
-        v_terminals = [c.split()[1:3] for c in self.list_components if c.startswith('V')]
+        v_terminals = np.array([' '.join(c.split()[1:3]) for c in self.list_components if c.startswith('V')])
         
         if len(v_terminals) > 1:
             new_list = copy(self.list_components)
-            mask = [t in v_terminals for t in v_terminals]
+            mask = np.array([True if np.sum(t == v_terminals) > 1 else False for t in v_terminals])
             for i,v in enumerate(v_sources[mask]):
                 v_components = v.split()
-                new_resistor = f'R9{i} N10{v_components[1]} {v_components[1]} 1000'
-                v_components[1] = f'N10{v_components[1]}'
+                new_resistor = f'R9{i} N10{i}{v_components[1]} {v_components[1]} 1000'
+                v_components[1] = f'N10{i}{v_components[1]}'
                 new_list.append(new_resistor)
                 new_list = [' '.join(v_components) if k == v else k for k in new_list]
             
         self.list_components = new_list
 
     def revalidate(self, components_in_node):
+        self.check_short_circuit_transistor()
         self.check_floating_nodes(components_in_node)
         self.check_parallel_sources()
                 
@@ -34,7 +58,7 @@ class Revalidator:
         
 
 if __name__ == '__main__':
-    list_components =  ['Q1 Out 0 5 0 NPN', 'R1 6 Out 56471', 'R2 0 5 62740',
+    list_components =  ['Q1 Out 5 5 0 NPN', 'R1 6 Out 56471', 'R2 0 5 62740',
                         'V1 5 0 9', 'V2 5 0 SINE(0 0.05 300)', 'R3 3 0 91614',
                         'R4 7 0 55723', 'C1 6 3 6.68u', 'C2 3 0 353.58u']
     
